f91614bbd672a6cea0ae519dbfae5c2ea07e11d6
Anthony <a.a.donovan@outlook.com>
2020-07-15 16:51:04 +0100

    adds tests for aged brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,20 +10,52 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].name).to eq 'foo'
     end
+
+    it 'decreases sell_in value by 1' do
+      items = [Item.new('foo', 10, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 9
+    end
   end
 
   describe '#Sulfuras' do
+    before(:each) do
+      @items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 10)]
+      GildedRose.new(@items).update_quality
+    end
+
     describe 'quality' do
-      it 'does not decrease before sell by date' do
-        items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 10)]
-        GildedRose.new(items).update_quality
-        expect(items[0].quality).to eq 10
+      it 'does not decrease before sell_in value' do
+        expect(@items[0].quality).to eq 10
       end
 
-      it 'does not decrease after sell by date' do
-        items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 10)]
-        11.times { GildedRose.new(items).update_quality }
-        expect(items[0].quality).to eq 10
+      it 'does not decrease after sell_in value' do
+        3.times { GildedRose.new(@items).update_quality }
+        expect(@items[0].quality).to eq 10
+      end
+    end
+  end
+
+  describe '#Aged Brie' do
+    before(:each) do
+      @items = [Item.new('Aged Brie', 2, 10)]
+      GildedRose.new(@items).update_quality
+    end
+
+    describe 'quality' do
+      it 'increases by 1 each day before sell_in value' do
+        GildedRose.new(@items).update_quality
+        expect(@items[0].quality).to eq 12
+      end
+
+      it 'increases by 2 each day after sell_in value' do
+        3.times { GildedRose.new(@items).update_quality }
+        expect(@items[0].quality).to eq 16
+      end
+
+      it 'has a mximum value of 50' do
+        51.times { GildedRose.new(@items).update_quality }
+        expect(@items[0].quality).to eq 50
       end
     end
   end
